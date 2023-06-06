34557d48425e3df3b8a7e9eca2599f23906016bd
Milica <milica.golubovic.4380@metropolitan.ac.rs>
2023-06-06 18:33:10 +0200

    Unlogged user can now see games

--- src/main/java/ac/rs/metropolitan/it355pz/security/SecurityConfiguration.java
+++ src/main/java/ac/rs/metropolitan/it355pz/security/SecurityConfiguration.java
@@ -2,12 +2,9 @@ package ac.rs.metropolitan.it355pz.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,44 +12,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
-    public static final String AUTH_ENDPOINT = "authenticate/**";
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-//    @Order(Ordered.HIGHEST_PRECEDENCE)
-//    @Bean
-//    protected SecurityFilterChain filterChain(HttpSecurity httpSecurity, JwtFilter jwtFilter) throws Exception {
-//        return httpSecurity
-//                .csrf()
-//                .disable()
-//                .cors().and()
-//                .authorizeHttpRequests()
-//                .requestMatchers("/games").permitAll()
-//                .requestMatchers(AUTH_ENDPOINT).permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .headers()
-//                .cacheControl()
-//                .and()
-//                .and().build();
-//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
